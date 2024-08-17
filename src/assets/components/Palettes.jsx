cc0d780470318f2d6aa1d8c69a2db84ed34e37cf
Ger Espinosa <espinosa.toscano@gmail.com>
2024-08-17 18:57:28 +0200

    Added color codes and zoom to stored palettes

--- src/assets/components/Palettes.jsx
+++ src/assets/components/Palettes.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 export const Palettes = () => {
     const [allCodes, setAllCodes] = useState([]);
+    const [isOpened, setIsOpened] = useState(false)
     const [savedPalettes, setSavedPalettes] = useState([]);
 
     useEffect(() => {
@@ -15,7 +16,18 @@ export const Palettes = () => {
 
     const handleSavePalette = () => {
         if (allCodes.length > 0) {
-            setSavedPalettes(prevPalettes => [...prevPalettes, allCodes]);
+            setSavedPalettes(prevPalettes => [...prevPalettes, [...allCodes]]);
+        }
+    };
+
+    const handleZoomPalette = (paletteIndex) => {
+        const selectedPallete = document.getElementById(`palette${paletteIndex}`);
+        if (selectedPallete && isOpened == false) {
+            selectedPallete.classList.add('modal');
+            setIsOpened(true)
+        } else {
+            setIsOpened(false)
+            selectedPallete.classList.remove('modal')
         }
     };
 
@@ -31,19 +43,27 @@ export const Palettes = () => {
                             key={index}
                             className='w-12 h-12'
                             style={{ backgroundColor: colorCode }}
-                        ></div>
+                        >
+                        </div>
                     ))}
                 </div>
             </div>
             <div className='flex-1 flex flex-wrap h-fit w-full mx-auto justify-center items-center p-4'>
-                {savedPalettes.map((palette, index) => (
-                    <div key={index} className='flex border-4 border-white mb-2'>
+                {savedPalettes.map((palette, paletteIndex) => (
+                    <div
+                        key={paletteIndex}
+                        className='flex border-4 border-white mb-2 cursor-pointer'
+                        id={`palette${paletteIndex}`}
+                        onClick={() => handleZoomPalette(paletteIndex)}
+                    >
                         {palette.map((colorCode, colorIndex) => (
                             <div
                                 key={colorIndex}
-                                className='w-6 h-8'
+                                className={`w-12 h-12 ${isOpened == false ? 'hover:scale-125' : 'hover:scale-x-200'} hover:text-white overflow-x-hidden flex justify-center items-center`}
                                 style={{ backgroundColor: colorCode }}
-                            ></div>
+                            >
+                                <p className={`opacity-100 text-xs`}>{colorCode}</p>
+                            </div>
                         ))}
                     </div>
                 ))}
